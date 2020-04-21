75204af00939343e07f1e2b3894b30757a7c0696
Cerila Canda <cerila.canda@codingavenue.com>
2020-04-21 21:01:27 +0800

    CC-727 Added the test files of the lesson

---
+++ tests/TernaryOperator/MissingDoubleQuotesOnOperandTest.php
@@ -0,0 +1,124 @@
+<?php
+use CodingAvenue\Proof\Code;
+use PHPUnit\Framework\TestCase;
+
+class MissingDoubleQuotesOnOperandTest extends TestCase
+{
+    protected static $code;
+
+    public static function setupBeforeClass()
+    {
+        self::$code = new Code(getcwd() . "/" . getenv("TEST_INDEX"));
+    }
+
+    public function testPhpStartTag()
+    {
+        $checkStart = self::$code->codeStartCheck();
+
+        $this->assertEquals(true, $checkStart, "Expecting the `<?php` tag on the first line.");
+    }
+
+    public function testActualCode()
+    {
+        $evaluator = self::$code->evaluator();
+        $evaled    = $evaluator->evaluate();
+        $expected  = "Needs Improvement";
+
+        $this->assertEquals($expected, $evaled['output'], "Expected output is \"$expected\".");
+    }
+
+    public function testEcho()
+    {
+        $nodes = self::$code->find('construct[name="echo"]');
+
+        $this->assertEquals(1, $nodes->count(), "Expecting one echo statement.");
+    }
+
+    public function testAssignment()
+    {
+        $nodes = self::$code->find('operator[name="assignment"]');
+        
+        $this->assertEquals(2, $nodes->count(), "Expecting two assignment statements.");
+    }
+
+    public function testStudentGradeVariable()
+    {
+        $nodes = self::$code->find('variable[name="studentGrade"]');
+
+        $this->assertEquals(2, $nodes->count(), "Expecting two occurrences of the variable named 'studentGrade'.");
+    }
+
+    public function testRemarksVariable()
+    {
+        $nodes = self::$code->find('variable[name="remarks"]');
+
+        $this->assertEquals(2, $nodes->count(), "Expecting two occurrences of the variable named 'remarks'.");
+    }
+
+    public function testVeryGoodValue()
+    {
+        $nodes = self::$code->find('string[value="Very Good"]');
+
+        $this->assertEquals(1, $nodes->count(), "Expecting a string 'Very Good'.");
+    }
+
+    public function testNeedsImprovementValue()
+    {
+        $nodes = self::$code->find('string[value="Needs Improvement"]');
+
+        $this->assertEquals(1, $nodes->count(), "Expecting a string 'Needs Improvement'.");
+    }
+
+    public function testRemarksOnEcho()
+    {
+        $nodes = self::$code->find('construct[name="echo"]');
+        $remarks = $nodes->find('variable[name="remarks"]');
+
+        $this->assertEquals(1, $remarks->count(), "Expecting one 'remarks' variable in the echo statement.");
+    }
+
+    public function testGreateThanOperator()
+    {
+        $nodes = self::$code->find('operator[name="greater"]');
+
+        $this->assertEquals(1, $nodes->count(), "Expecting one greater than operator.");
+    }
+
+    public function testStudentGradeOnCondition()
+    {
+        $nodes = self::$code->find('operator[name="greater"]');
+        $left = $nodes->getSubNode('left'); 
+        $var = $left->find('variable[name="studentGrade"]');
+
+        $this->assertEquals(1, $var->count(), "Expecting one 'studentGrade' variable in the conditional expression.");
+    }
+
+    public function testValueOnCondition()
+    {
+        $nodes = self::$code->find('operator[name="greater"]');
+        $left = $nodes->getSubNode('left'); 
+        $num = $left->find('integer'); // nice to have 'integer[value=75]' 
+
+        $this->assertEquals(1, $num->count(), "Expecting one integer value in the conditional expression.");
+    }
+
+    public function testStudentGradeOnAssignment()
+    {
+        $nodes = self::$code->find('operator[name="assignment"]');
+        $subNode = $nodes->getSubNode();
+        $var = $subNode->find('variable[name="studentGrade"]');
+
+        $this->assertEquals(2, $var->count(), "Expecting two occurrences of the 'studentGrade' variable in an assignment statement.");
+    }
+
+    public function testRemarksOnAssignment()
+    {
+        $nodes = self::$code->find('operator[name="assignment"]');
+        $subNode = $nodes->getSubNode();
+        $var = $subNode->find('variable[name="remarks"]');
+
+        $this->assertEquals(1, $var->count(), "Expecting one occurrence of the 'remarks' variable in an assignment statement.");
+    }
+
+    // still needs ternary operator validation here...
+}
---
+++ tests/TernaryOperator/UseTernaryOperatorTest.php
@@ -0,0 +1,124 @@
+<?php
+use CodingAvenue\Proof\Code;
+use PHPUnit\Framework\TestCase;
+
+class UseTernaryOperatorTest extends TestCase
+{
+    protected static $code;
+
+    public static function setupBeforeClass()
+    {
+        self::$code = new Code(getcwd() . "/" . getenv("TEST_INDEX"));
+    }
+
+    public function testPhpStartTag()
+    {
+        $checkStart = self::$code->codeStartCheck();
+
+        $this->assertEquals(true, $checkStart, "Expecting the `<?php` tag on the first line.");
+    }
+
+    public function testActualCode()
+    {
+        $evaluator = self::$code->evaluator();
+        $evaled    = $evaluator->evaluate();
+        $expected  = "positive";
+
+        $this->assertEquals($expected, $evaled['output'], "Expected output is \"$expected\".");
+    }
+
+    public function testEcho()
+    {
+        $nodes = self::$code->find('construct[name="echo"]');
+
+        $this->assertEquals(1, $nodes->count(), "Expecting one echo statement.");
+    }
+
+    public function testAssignment()
+    {
+        $nodes = self::$code->find('operator[name="assignment"]');
+        
+        $this->assertEquals(2, $nodes->count(), "Expecting two assignment statements.");
+    }
+
+    public function testNumberVariable()
+    {
+        $nodes = self::$code->find('variable[name="number"]');
+
+        $this->assertEquals(2, $nodes->count(), "Expecting two occurrences of the variable named 'number'.");
+    }
+
+    public function testResultVariable()
+    {
+        $nodes = self::$code->find('variable[name="result"]');
+
+        $this->assertEquals(2, $nodes->count(), "Expecting two occurrences of the variable named 'result'.");
+    }
+
+    public function testPositiveValue()
+    {
+        $nodes = self::$code->find('string[value="positive"]');
+
+        $this->assertEquals(1, $nodes->count(), "Expecting a string 'positive'.");
+    }
+
+    public function testNegativeValue()
+    {
+        $nodes = self::$code->find('string[value="negative"]');
+
+        $this->assertEquals(1, $nodes->count(), "Expecting a string 'negative'.");
+    }
+
+    public function testResultOnEcho()
+    {
+        $nodes = self::$code->find('construct[name="echo"]');
+        $result = $nodes->find('variable[name="result"]');
+
+        $this->assertEquals(1, $result->count(), "Expecting one 'result' variable in the echo statement.");
+    }
+
+    public function testGreateThanEqualOperator()
+    {
+        $nodes = self::$code->find('operator[name="greater-equal"]');
+
+        $this->assertEquals(1, $nodes->count(), "Expecting one greater than or equal to operator.");
+    }
+
+    public function testNumberOnCondition()
+    {
+        $nodes = self::$code->find('operator[name="greater-equal"]');
+        $left = $nodes->getSubNode('left'); 
+        $var = $left->find('variable[name="number"]');
+
+        $this->assertEquals(1, $var->count(), "Expecting one 'number' variable in the conditional expression.");
+    }
+
+    public function testValueOnCondition()
+    {
+        $nodes = self::$code->find('operator[name="greater-equal"]');
+        $left = $nodes->getSubNode('left'); 
+        $num = $left->find('integer'); // nice to have 'integer[value=75]' 
+
+        $this->assertEquals(1, $num->count(), "Expecting one integer value in the conditional expression.");
+    }
+
+    public function testNumberOnAssignment()
+    {
+        $nodes = self::$code->find('operator[name="assignment"]');
+        $subNode = $nodes->getSubNode();
+        $var = $subNode->find('variable[name="number"]');
+
+        $this->assertEquals(2, $var->count(), "Expecting two occurrences of the 'number' variable in an assignment statement.");
+    }
+
+    public function testResultOnAssignment()
+    {
+        $nodes = self::$code->find('operator[name="assignment"]');
+        $subNode = $nodes->getSubNode();
+        $var = $subNode->find('variable[name="result"]');
+
+        $this->assertEquals(1, $var->count(), "Expecting one occurrence of the 'result' variable in an assignment statement.");
+    }
+
+    // still needs ternary operator validation here...
+}
