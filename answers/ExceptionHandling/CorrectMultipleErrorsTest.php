b9bf74f5ac88e77034e82b169356e83eb64b4ba9
Cerila Canda <cerila.canda@codingavenue.com>
2020-05-04 13:32:18 +0800

    CC-733 Added answers files of the lesson

---
+++ answers/ExceptionHandling/CorrectMultipleErrorsTest.php
@@ -0,0 +1,20 @@
+<?php
+function divide($dividend, $divisor)
+{
+    if (!is_numeric($divisor) || !is_numeric($dividend)) {
+        throw new InvalidArgumentException("Invalid argument value provided");
+    } elseif ($divisor == 0) {
+        throw new Exception("The value '0' as a divisor is not allowed.");
+    } else {
+        return ($dividend / $divisor);
+    }
+}
+
+try {
+    echo divide(10, "hello");
+} catch (InvalidArgumentException $ie) {
+    echo "Exception caught: " . $ie->getMessage() . " in " . $ie->getFile() . " on line number " . $ie->getLine();
+} catch (Exception $e) {
+    echo "Exception caught: [" . $e->getCode() . "] " . $e->getMessage();
+}
+?>
---
+++ answers/ExceptionHandling/MissingNewInTheThrowStatementTest.php
@@ -0,0 +1,16 @@
+<?php
+function divide($dividend, $divisor)
+{
+    if ($divisor == 0) {
+        throw new Exception("The value '0' as a divisor is not allowed.");
+    } else {
+        return ($dividend / $divisor);
+    }
+}
+
+try {
+    echo divide(10, 0);
+} catch (Exception $e) {
+    echo "Exception caught: " . $e->getMessage();
+}
+?>
